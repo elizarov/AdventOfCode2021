e30882d194ac39a68d8ae1b1b3c72834aab6d6bc
Roman Elizarov <elizarov@gmail.com>
2021-12-04 08:21:21 +0300

    Day04

---
+++ src/Day04_2.kt
@@ -0,0 +1,45 @@
+fun main() {
+    val dayId = "04"
+    val input = readInput("Day${dayId}")
+    val nums = input[0].split(",").map { it.toInt() }
+    class Brd(ls: List<String>) {
+        val a = ls.map { l -> l.trim().split(" ").filter { it != "" }.map { it.toInt() } }
+        val m = Array(5) { BooleanArray(5) }
+        val r = IntArray(5)
+        val c = IntArray(5)
+
+        fun mark(x: Int): Boolean {
+            for (i in 0..4) for (j in 0..4) {
+                if (a[i][j] == x && !m[i][j]) {
+                    m[i][j] = true
+                    r[i]++
+                    c[j]++
+                }
+            }
+            return r.any { it == 5 } || c.any { it == 5 }
+        }
+
+        fun score(): Int {
+            var s = 0
+            for (i in 0..4) for (j in 0..4) {
+                if (!m[i][j]) s += a[i][j]
+            }
+            return s
+        }
+    }
+    val bs = ArrayList<Brd>()
+    for (r in 2 until input.size step 6) {
+        bs += Brd(input.subList(r, r + 5))
+    }
+    var ans = 0
+    val w = BooleanArray(bs.size)
+    for (x in nums) {
+        for ((i, b) in bs.withIndex()) if (!w[i]) {
+            if (b.mark(x)) {
+                w[i] = true
+                ans = x * b.score()
+            }
+        }
+    }
+    println(ans)
+}
